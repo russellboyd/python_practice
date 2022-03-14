cc9fa4aca3a938824057d78fca4f1d14496d07c6
russellboyd <russellgboyd@icloud.com>
2022-03-13 23:10:37 -0500

    Create loopy_string.py

---
+++ loopy_string.py
@@ -0,0 +1,10 @@
+#Loopy String
+#demonstrates the for loop with a string
+
+word = input("Enter a word: ")
+
+print("\nHere's each letter in your word:")
+for letter in word:
+    print(letter);
+
+input("\n\nPress the enter key to exit.");
